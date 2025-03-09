0184161d627a1bb571bf4d921a8dce0fc5e25221
raedalh <raed.hammami@gmail.com>
2025-03-10 00:31:56 +0300

    Updated build files and configurations

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 export default {
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    moduleDirectories: ['node_modules', 'src/client/js'],
-  };
+  testEnvironment: 'jsdom', // Use jsdom for browser-like environment
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock CSS imports
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest', // Use Babel for JavaScript files
+  },
+  moduleDirectories: ['node_modules', 'src/client/js'], // Look for modules in these directories
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: 'coverage', // Output directory for coverage reports
+  coverageReporters: ['html', 'text'], // Generate HTML and text reports
+  collectCoverageFrom: [
+    'src/client/js/**/*.js', // Include all JS files in src/client/js
+    '!src/client/js/**/*.test.js', // Exclude test files
+  ],
+};
